b4ed8c20493e627e719c83690b091b6698a021a3
JonnyArm <130648163+JonnyArm@users.noreply.github.com>
2025-04-07 09:49:44 +0100

    Add files via upload

---
+++ client/lib/model/users.dart
@@ -0,0 +1,24 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'Users.g.dart';
+
+@JsonSerializable()
+class Users {
+  int id;
+  String? nome;
+  String? cognome;
+  String username;
+  String? token;
+
+  Users({
+    required this.id,
+    this.nome,
+    this.cognome,
+    required this.username,
+    this.token,
+  });
+
+  factory Users.fromJson(Map<String, dynamic> json) => _$UsersFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UsersToJson(this);
+}
